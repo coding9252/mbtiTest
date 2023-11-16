525305311c67d283fa8bf1b629c6610cca5de73e
Gahyeon <coding9252@naver.com>
2023-11-16 20:22:26 +0900

    3주차 3강

--- app/src/main/java/com/leegahyeon/mbtitest/ViewPagerAdapter.kt
+++ app/src/main/java/com/leegahyeon/mbtitest/ViewPagerAdapter.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(FragmentActivity)  {
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity)  {
     override fun getItemCount(): Int {
         return 4
     }
